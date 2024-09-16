14d4597f42915690a6284f693a61bc9c2aa1b851
muhammad-saad-imran <muhammad.saad02@arbisoft.com>
2024-09-16 14:00:01 +0500

    push package.json

---
+++ dist/database/models/Client.js
@@ -0,0 +1,49 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
+const __1 = __importDefault(require(".."));
+const Order_1 = __importDefault(require("./Order"));
+class Client extends sequelize_1.Model {
+}
+/**
+ * Initialize Client model
+ */
+Client.init({
+    id: {
+        type: sequelize_1.DataTypes.UUID,
+        defaultValue: sequelize_1.UUIDV4,
+        primaryKey: true,
+    },
+    name: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: true,
+    },
+    phoneNumber: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: true,
+    },
+    createdAt: {
+        type: sequelize_1.DataTypes.DATE,
+    },
+    updatedAt: {
+        type: sequelize_1.DataTypes.DATE,
+    },
+}, {
+    sequelize: __1.default,
+    tableName: "clients",
+    timestamps: true,
+});
+/**
+ * Client associations
+ */
+Client.hasMany(Order_1.default, {
+    foreignKey: "clientId",
+});
+exports.default = Client;
---
+++ dist/database/models/Supplier.js
@@ -0,0 +1,49 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
+const __1 = __importDefault(require(".."));
+const Inventory_1 = __importDefault(require("./Inventory"));
+class Supplier extends sequelize_1.Model {
+}
+/**
+ * Initialize Supplier model
+ */
+Supplier.init({
+    id: {
+        type: sequelize_1.DataTypes.UUID,
+        defaultValue: sequelize_1.UUIDV4,
+        primaryKey: true,
+    },
+    name: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+    },
+    phoneNumber: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+    },
+    createdAt: {
+        type: sequelize_1.DataTypes.DATE,
+    },
+    updatedAt: {
+        type: sequelize_1.DataTypes.DATE,
+    },
+}, {
+    sequelize: __1.default,
+    tableName: "supplier",
+    timestamps: true,
+});
+/**
+ * Supplier associations
+ */
+Supplier.hasMany(Inventory_1.default, {
+    foreignKey: "supplierId",
+});
+exports.default = Supplier;
